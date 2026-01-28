e00976aad874b0a829d970d0da2924e5654d9de0
feuerswut <80217805+feuerswut@users.noreply.github.com>
2026-01-28 21:10:08 +0100

    Update plugin.js / Version so it shows up / thanks @rejetto

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,9 +1,9 @@
 // Plugin metadata for HFS v3
-exports.version = '1.2';
-exports.description = 'System Statistics Dashboard - Real-time monitoring of CPU, memory, disk, temperature and network stats';
-exports.apiRequired = '8.65'; // ctx API version
+exports.version = 1.4;
+exports.description = "System Statistics Dashboard - Real-time monitoring of CPU, memory, disk, temperature and network stat";
+exports.apiRequired = 8.65;
 
-exports.author = 'Feuerswut';
+exports.author = "Feuerswut";
 exports.repo = "Feuerswut/hfs-sysstats"
 
 exports.config = {
